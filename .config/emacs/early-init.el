187f14a4033b31a7fb8dbb04090fac54c74d0212
rxf4e1 <rxf4e1@pm.me>
2023-11-10 17:06:14 -0300

    Initial commit

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,83 @@
+;;; Package: --- early-init.el -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(setq package-enable-at-startup nil)
+(setq inhibit-default-init nil)
+
+(setq native-comp-async-report-warnings-errors nil)
+
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name ".local/var/eln-cache/" user-emacs-directory))))
+
+;; (unless (string-empty-p file)
+;;   (eval-after-load file
+;;     '(debug)))
+
+;; (setq debug-on-message "")
+;; (add-variable-watcher 'org-capture-after-finalize-hook
+;;                       (lambda (symbol newval operation where)
+;;                         (debug)
+;;                         (message "%s set to %s" symbol newval)))
+;; (setq debug-on-error t)
+
+
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 1)
+
+(defun +gc-after-focus-change ()
+  "Run GC when frame loses focus."
+  (run-with-idle-timer
+   5 nil
+   (lambda () (unless (frame-focus-state) (garbage-collect)))))
+
+(defun +reset-init-values ()
+  (run-with-idle-timer
+   1 nil
+   (lambda ()
+     (setq file-name-handler-alist default-file-name-handler-alist
+           gc-cons-percentage 0.1
+           gc-cons-threshold 100000000)
+     (message "gc-cons-threshold & file-name-handler-alist restored")
+     (when (boundp 'after-focus-change-function)
+       (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
+
+(with-eval-after-load 'elpaca
+  (add-hook 'elpaca-after-init-hook '+reset-init-values))
+
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize t
+      use-dialog-box t ; only for mouse events, which I seldom use
+      use-file-dialog nil
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      inhibit-startup-echo-area-message user-login-name ; read the docstring
+      inhibit-startup-buffer-menu t)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(push '(font . "Terminess Nerd Font Mono-9") default-frame-alist)
+(set-face-font 'default "Terminess Nerd Font Mono-9")
+(set-face-font 'variable-pitch "Terminess Nerd Font Mono-9")
+(copy-face 'default 'fixed-pitch)
+
+(setq server-client-instructions nil)
+(setq frame-inhibit-implied-resize t)
+
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq desktop-restore-forces-onscreen nil)
+
+(setq ring-bell-function #'ignore
+      inhibit-startup-screen t)
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ .config/emacs/init.el
@@ -0,0 +1,81 @@
+;;; Package: --- init.el -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(add-hook 'elpaca-after-init-hook
+          (lambda ()
+            (message "Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract (current-time) before-init-time)))
+                     gcs-done)))
+
+(defvar elpaca-installer-version 0.6)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (call-process "git" nil buffer t "clone"
+                                       (plist-get order :repo) repo)))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+;; (profiler-start 'cpu+mem)
+;; (add-hook 'elpaca-after-init-hook (lambda () (profiler-stop) (profiler-report)))
+
+;; Some basic settings
+(setq frame-title-format '("%b"))
+(setq ring-bell-function 'ignore)
+(setq use-short-answers t)
+(setq native-comp-async-report-warnings-errors 'silent) ; EMACS 28 WITH NATIVE COMPILATION
+(setq native-compile-prune-cache t) ; Emacs 29
+(setq make-backup-files nil)
+(setq backup-inhibited nil) ; Not sure if needed, given `make-backup-files'
+(setq create-lockfiles nil)
+
+;; Always start with *scratch*
+(setq initial-buffer-choice t)
+
+;; load config file
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "./config.org" user-emacs-directory))
+(add-hook 'elpaca-after-init-hook (lambda () (load-file "./config.el")))
+
+;; (let ((config-dot-el (concat user-emacs-directory "config.el"))
+;;       (config-dot-org (concat user-emacs-directory "config.org")))
+;;   (if (not (file-exists-p config-dot-el))
+;;       (org-babel-tangle-file config-dot-org))
+;;   (load-file config-dot-el))
+
+;;END OF INIT FILE
+;; (setq elpaca-after-init-time (current-time)) ;; prevents `elpaca-after-init-hook` from running later.
+
+(provide 'init)
+;;; init.el ends here.
